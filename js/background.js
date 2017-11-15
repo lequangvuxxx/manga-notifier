0e152f15f0b257706fe04a94dafd6955660a0ee5
Le Quang Vu <l.q.v.x.x.x.99@gmail.com>
2017-11-16 01:25:00 +0700

    Testing...

--- js/background.js
+++ js/background.js
@@ -1,32 +1,114 @@
 
+function updateSupportedSites() {
+    $.get('https://github.com/lequangvuxxx/manga-notifier/raw/master/supported-sites.json?raw=True')
+        .done(dat => {
+            chrome.storage.local.set({'supported-sites': JSON.parse(dat)});
+        })
+}
 chrome.runtime.onInstalled.addListener((dt) => {
-	if(dt.reason == "install") {
-		chrome.storage.local.get((d)=>{
-			if(!d.hasOwnProperty('supported-sites')) {
-				chrome.storage.local.set('supported-sites', {});
-			}
-		})
-	}
+    if(dt.reason == 'install')
+        updateSupportedSites();
 })
 
-getNotifications();
-function getNotifications() {
-
-    chrome.storage.local.get(data => {
-
-        if(!data.hasOwnProperty('bookmarks')) return;
-        data.bookmarks.forEach(link => {
-            console.log(link);
-
-            // $.get(link)
-            //     .done(page => {
-            //
-            //     })
-            //     .fail(() => {
-            //     })
-            // chrome.browserAction.setBadgeBackgroundColor({color: "#115100"});
-            // chrome.browserAction.setBadgeText({text:t});
-        })
+var siteDetails;
+(function () {
+    chrome.storage.local.get('supported-sites', dat => {
+        siteDetails = dat['supported-sites'].details
     })
-	setTimeout(getNotifications, 1000*60*2);
+})();
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+
+    if(!request.type) return;
+    if(request.type == "GetNotification") {
+        //chrome.tabs.create({ url: chrome.runtime.getURL("index.html")});
+    } else
+    if(request.type == "NewBookmark") {
+        const url  = request.url;
+        const site = request.site;
+        const name = request.name;
+        const thumbnail = siteDetails[site].thumbnail;
+
+        $.get(url)
+            .done(page => {
+                const img = $(page).find(thumbnail);
+                if(img.length == 0) return;
+                const imgSrc = img.attr('src');
+                createThumbnail(imgSrc, newBookmark(name, url, site))
+            })
+    }
+})
+
+function createThumbnail(url, callback) {
+    $.get('http://uploads.im/api?upload='+url+'&resize_width=100&thumb_width=50&format=json')
+        .done(
+            res => callback(res.data.img_url)
+        )
+}
+
+function newBookmark(name, url, site) {
+    return function(thumbnail) {
+        chrome.storage.local.get('bookmarks', res => {
+            const bookmarks = res.bookmarks || [];
+            bookmarks.push({
+                url,
+                name,
+                site,
+                thumbnail
+            })
+            chrome.storage.local.set({bookmarks});
+        })
+    }
+}
+
+function getNotification() {
+    chrome.storage.local.get(data => {
+        if(data.hasOwnProperty('bookmarks')) {
+            let newNotis  = 0;
+            let checkedBM = 0;
+            const totalBM = data.bookmarks.length;
+            data.bookmarks.forEach(item =>
+                checkForUpdate(item, gotNew => {
+
+                    checkedBM++;
+
+                    if(!gotNew) return;
+
+                    newNotis++;
+                    addToNoti(item, gotNew);
+
+                    if(checkedBM < totalBM) return;
+
+                    if(newNotis == 0) newNotis = "";
+                    chrome.browserAction.setBadgeBackgroundColor({color: "#115100"});
+                    chrome.browserAction.setBadgeText({text: newNotis});
+                })
+            );
+        }
+    })
+	setTimeout(getNotification, 1000*60*2);
+}
+getNotification();
+
+function checkForUpdate({site, url}, callback) {
+
+    const listSel = siteDetails[site].list;
+
+    $.get(url)
+    .done(page => {
+
+        const list = $(page).find(listSel);
+
+        if(list.length == 0) return;
+
+        const lastedChap = site + list[0].href;
+
+        chrome.history.getVisited({url: lastedChap}, arr => {
+            callback(arr.length == 0 ? false : list[0].title);
+        });
+    })
+}
+
+function addToNoti(bookmark, newInfo) {
+
 }
--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,58 @@
 
+var bookmarkContain, notificationContain;
+function buildBookmark(tab, sites) {
+	bookmarkContain = {tab, sites, html:$('div.container').html()};
+
+	$('#name').val(tab.title).focus().select();
+
+	for(const site in sites) {
+		if (tab.url.startsWith(site)) {
+			$('#options').html(
+				'<button> Hủy </button>' +
+				'<button class="green">Xác nhận</button>'
+			)
+			$('#options > button.green')
+				.click(() => {
+					chrome.runtime.sendMessage({
+						type:"NewBookmark",
+						name: $('#name').val(),
+						url: tab.url,
+						site
+					})
+				})
+			return;
+		}
+	}
+	return $('#options').html('<p class="warning">Hiện tại chưa hỗ trợ trang này!</p>')
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-
 	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		chrome.storage.local.get('supported-sites', data => {
+			buildBookmark(tabs[0], data['supported-sites'].details);
+		})
+	})
+	$('#notification').click(function() {
+		$('div.nav > ul > li').removeClass('active');
+		$(this.parentNode).addClass('active');
 
-		let tab = tabs[0];
-		$('#name').val(tab.title).focus().select();
+		if(notificationContain)
+			return $('div.container').html(notificationContain);
+
+		chrome.runtime.sendMessage({type:"GetNotification"},(res)=> {
+			notificationContain = res;
+			$('div.container').html(notificationContain);
+		})
+	})
+	$('#bookmark').click(function() {
+		$('div.nav > ul > li').removeClass('active');
+		$(this.parentNode).addClass('active');
+		if(bookmarkContain) {
+			$('div.container').html(bookmarkContain.html);
+			buildBookmark(
+				bookmarkContain.tab,
+				bookmarkContain.sites
+			);
+		}
 	})
 })
